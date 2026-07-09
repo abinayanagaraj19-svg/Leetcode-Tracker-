5165a96390f7faec42ff3ee0fb3f21e815b8b6c8
abinayanagaraj19-svg <abinayanagaraj19@gmail.com>
2026-07-09 15:07:19 +0530

    Create 2238-ANumberAfterADoubleReversal

---
+++ 2238-ANumberAfterADoubleReversal/2238-ANumberAfterADoubleReversal.java
@@ -0,0 +1,9 @@
+// Last updated: 09/07/2026, 15:07:20
+class Solution {
+    public boolean isSameAfterReversals(int num) {
+        if (num==0)return true;
+        if (num%10==0)return false;
+        else return true;
+        
+    }
+}
