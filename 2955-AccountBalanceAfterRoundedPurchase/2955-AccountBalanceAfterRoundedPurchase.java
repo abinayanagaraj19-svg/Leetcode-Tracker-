ddb28faa46b86e1c153a25bf0f74d9d8169e488b
abinayanagaraj19-svg <abinayanagaraj19@gmail.com>
2026-07-09 15:06:53 +0530

    Create 2955-AccountBalanceAfterRoundedPurchase

---
+++ 2955-AccountBalanceAfterRoundedPurchase/2955-AccountBalanceAfterRoundedPurchase.java
@@ -0,0 +1,8 @@
+// Last updated: 09/07/2026, 15:06:54
+class Solution {
+    public int accountBalanceAfterPurchase(int purchaseAmount){
+        return 100 -((purchaseAmount+5)/10)*10;
+
+        
+    }
+}
