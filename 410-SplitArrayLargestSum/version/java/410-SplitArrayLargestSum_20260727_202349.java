b8609e0d0c6b949c49273bfe79985cec01f54c8d
abinayanagaraj19-svg <abinayanagaraj19@gmail.com>
2026-07-27 20:23:49 +0530

    Create 410-SplitArrayLargestSum

---
+++ 410-SplitArrayLargestSum/version/java/410-SplitArrayLargestSum_20260727_202349.java
@@ -0,0 +1,19 @@
+// Last updated: 27/07/2026, 20:23:49
+1class Solution {
+2    public int maxProfit(int[] prices) {
+3
+4        int minPrice = Integer.MAX_VALUE;
+5        int maxProfit = 0;
+6
+7        for (int price : prices) {
+8
+9            if (price < minPrice) {
+10                minPrice = price;
+11            } else {
+12                maxProfit = Math.max(maxProfit, price - minPrice);
+13            }
+14        }
+15
+16        return maxProfit;
+17    }
+18}
