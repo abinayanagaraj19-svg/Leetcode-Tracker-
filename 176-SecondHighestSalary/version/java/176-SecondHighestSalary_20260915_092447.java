c81b95777b4b930957d868a29e7e7252955ba18d
Abinaya N <abinayanagaraj19@gmail.com>
2026-09-15 09:24:48 +0530

    Create 176-SecondHighestSalary

---
+++ 176-SecondHighestSalary/version/java/176-SecondHighestSalary_20260915_092447.java
@@ -0,0 +1,25 @@
+// Last updated: 15/09/2026, 09:24:47
+1class Solution {
+2    public int findMin(int[] nums) {
+3
+4        int left = 0;
+5        int right = nums.length - 1;
+6
+7        while (left < right) {
+8
+9            int mid = left + (right - left) / 2;
+10
+11            if (nums[mid] > nums[right]) {
+12                left = mid + 1;
+13            }
+14            else if (nums[mid] < nums[right]) {
+15                right = mid;
+16            }
+17            else {
+18                right--;
+19            }
+20        }
+21
+22        return nums[left];
+23    }
+24}
